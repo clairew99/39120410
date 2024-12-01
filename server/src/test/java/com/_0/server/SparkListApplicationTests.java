8e7c0ac5e2b8d6dd839d23dd87325831cf06194d
이은우 <clairew.tech@gmail.com>
2024-12-01 21:05:53 +0900

    settings : 프로젝트 생성

---
+++ server/src/test/java/com/_0/server/SparkListApplicationTests.java
@@ -0,0 +1,13 @@
+package com._0.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
